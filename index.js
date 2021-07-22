a060f788109b0b92d972303897fc9455a8fd3b17
Masab <Masabk214@gmail.com>
2021-07-22 05:18:17 -0700

    adding first two function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('working');
+
+let paramsRadio = document.getElementById('paramsRadio').addEventListener('click',()=>{
+  document.getElementById('requestJsonBox').style.display='none';
+  document.getElementById('parametersBox').style.display='block';
+
+})
+
+let jsonRadio = document.getElementById('jsonRadio').addEventListener('click',()=>{
+    document.getElementById('parametersBox').style.display='none';
+    document.getElementById('requestJsonBox').style.display='block';
+
+  })
