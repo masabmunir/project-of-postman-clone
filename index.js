b51d455867db905a2452796b78a2827f59a81944
Masab <Masabk214@gmail.com>
2021-07-25 04:54:56 -0700

    adding of submit function and GET method in JS file

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ let addParam = document.getElementById('addParam').addEventListener('click', () 
        <input type="email" class="form-control" id="parameterKey${addedParamCount + 2}" placeholder="Enter Parameter ${addedParamCount + 2} Key">
    </div>
    <div class="form-group col-md-4">
-       <input type="password" class="form-control" id="parameterValue${addedParamCount + 2}" placeholder="Enter Parameter ${addedParamCount + 2} value">
+       <input type="text" class="form-control" id="parameterValue${addedParamCount + 2}" placeholder="Enter Parameter ${addedParamCount + 2} value">
    </div>
    <button id="addParam" class="btn btn-primary deleteParams">-</button>
 </div>`
@@ -40,11 +40,53 @@ let addParam = document.getElementById('addParam').addEventListener('click', () 
 
   let deleteParams = document.getElementsByClassName('deleteParams');
   for (item of deleteParams) {
-   item.addEventListener('click',(e)=>{
-     e.target.parentElement.remove();
-   })
+    item.addEventListener('click', (e) => {
+      e.target.parentElement.remove();
+    })
   }
   addedParamCount++;
 })
 
 
+let submit = document.getElementById('submit').addEventListener('click', () => {
+  document.getElementById('responseJsonText').value = "Wait Fetching Response.."
+
+  let url = document.getElementById('url').value;
+  let requestType = document.querySelector("input[name='requestType']:checked").value;
+  let contentType = document.querySelector("input[name='contentType']:checked").value;
+
+
+  if (contentType == 'params') {
+    data = {};
+    for (let i = 0; i < addedParamCount + 1; i++) {
+      if (document.getElementById('parameterKey' + (i + 1)) != undefined) {
+        let key = document.getElementById('parameterKey' + (i + 1)).value;
+        let value = document.getElementById('parameterValue' + (i + 1)).value;
+        data[key] = value;
+      }
+    }
+    data = JSON.stringify(data);
+  }
+  else {
+    data = document.getElementById('responseJsonText').value
+  }
+  console.log('URL is ', url);
+  console.log('requestType is ', requestType);
+  console.log('contentType is ', contentType);
+  console.log('data is ', data);
+
+  if (requestType == 'GET') {
+    fetch(url, {
+      method: 'GET',
+    })
+      .then(response => response.text())
+      .then((text) => {
+        document.getElementById('responseJsonText').value = text;
+        // document.getElementById('responsePrism').innerHTML = text;
+        // Prism.highlightAll();
+      });
+  }
+
+
+
+})
